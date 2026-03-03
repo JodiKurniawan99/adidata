c96b942c7d02f9984e6c98e1ee0d15ad87ed6b94
Jodi Kurniawan <49111430+JodiKurniawan99@users.noreply.github.com>
2026-03-03 20:49:52 +0700

    Add Occupation form screen with StateFlow ViewModel and tests

--- app/src/main/java/com/example/adidata/MainActivity.kt
+++ app/src/main/java/com/example/adidata/MainActivity.kt
@@ -4,44 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
 import com.example.adidata.ui.theme.AdidataTheme
 
 class MainActivity : ComponentActivity() {
+    private val occupationFormViewModel: OccupationFormViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AdidataTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                OccupationFormScreen(viewModel = occupationFormViewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AdidataTheme {
-        Greeting("Android")
     }
 }
